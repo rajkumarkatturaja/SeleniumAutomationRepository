f5195d14a531c079a8d111e897c83202dd37c227
Rajkumar <rajkumar.katturaja@gmail.com>
2026-05-27 16:48:37 -0400

    3rd commit

--- src/test/java/testCases/TC004_PlaceholderMethod.java
+++ src/test/java/testCases/TC004_PlaceholderMethod.java
@@ -1,6 +1,10 @@
 package testCases;
 
 public class TC004_PlaceholderMethod {
-  System.Out.println("This line is added from GitHub central repository");
+	
+	void test()
+	{
+  System.out.println("This line is added from GitHub central repository");
+	}
 
 }
